471be6c9a5f836e4605ec400d800f1d3d8777a79
JamirGVTimes <muhumuzajamir2016@gmail.com>
2022-06-24 09:07:35 +0300

    Second commit

--- src/catalogue/controller.js
+++ src/catalogue/controller.js
@@ -1,14 +1,14 @@
 import Order from './modal.js';
 import { Res } from '../util.js';
 import dotenv from 'dotenv';
-import nodemailer from 'nodemailer';
-import { google } from 'googleapis';
+//import nodemailer from 'nodemailer';
+//import { google } from 'googleapis';
 dotenv.config();
 
-const CLIENT_ID = process.env.client_id;
-const CLIENT_SECRET = process.env.client_sec;
-const REDIRECT_URI = process.env.redirect_url;
-const REFRESH_TOKEN = process.env.refresh_token;
+//const CLIENT_ID = process.env.client_id;
+//const CLIENT_SECRET = process.env.client_sec;
+//const REDIRECT_URI = process.env.redirect_url;
+//const REFRESH_TOKEN = process.env.refresh_token;
 
 const Controller = { 
   createOne: (req, res) => {
@@ -51,47 +51,8 @@ const Controller = {
       } else{
         const OrderTarget = await Order.findByIdAndUpdate(req.params.id, req.body)
         if (OrderTarget) {
-          const data = await Order.findById(req.params.id)
-         
-            res.status(200).send({ status: 200, message: 'Order Status changed successfully', data });
-          const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
-          oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
-          async function sendMail() {
-              try {
-                  const accessToken = await oAuth2Client.getAccessToken()
-
-                  const transport = nodemailer.createTransport({
-                      service: 'gmail',
-                      auth: {
-                          type: 'OAuth2',
-                          user: 'jamirafghanistan@gmail.com',
-                          clientId: CLIENT_ID,
-                          refreshToken: REFRESH_TOKEN,
-                          clientSecret: CLIENT_SECRET,
-                          accessToken: accessToken
-                      }
-                  });
-
-                          const mailOptions = {
-                              from: 'AirDady Shopwize API <jamirafghanistan@gmail.com>',
-                              to: OrderSelector.userId,
-                              subject: 'YOUR ORDER STATUS CHANGED',
-                            html: `<h3> Hello ${OrderSelector.userId} </h3>
-                               <p>We're glad to inform you that Your Order Status has changed to <b> ${req.body.status} </b>
-                               for order number <b> ${req.params.id} </b>
-                               </p>`
-                          };
-                        
-                  const result = await transport.sendMail(mailOptions)
-                  return result;
-                  
-              } catch (error) {
-                  return error
-              }
-          }
-          sendMail().then(result => console.log('Email Sent ...', result)).catch(error=>console.log(error.message))
-          
-          
+          const data = await Order.findById(req.params.id)         
+            res.status(200).send({ status: 200, message: 'Order Status changed successfully', data });       
         }
         return res.status(404).send({ status: 404, message: 'Order not found' })
 
